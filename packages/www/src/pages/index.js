8960d323cd3e2880cd7d30f7a3ea8e2235bd468a
Karthikeyan <karthikeyan@bangthetable.com>
2021-09-11 23:41:07 +0530

    chore: adding netlify identity

--- packages/www/src/pages/index.js
+++ packages/www/src/pages/index.js
@@ -1,10 +1,25 @@
-import React from 'react'
+import React,{useEffect} from 'react'
 import { Container, Heading, Button } from '@theme-ui/components';
+import netlifyIdentity from 'netlify-identity-widget'
+
+
+
 const App = () => {
+
+    useEffect(() => {
+        // running it on the client not on the server
+        netlifyIdentity.init({})    
+    })
+
+    const handleLogin = () => {
+        netlifyIdentity.open()
+    }
+
     return(
        <Container>
             <Heading as="h1"> Demo </Heading>
-            <Button> Login </Button>
+            <Button onClick={handleLogin}> Login </Button>
+            <Button onClick={() => console.log(netlifyIdentity.currentUser())}> Log User</Button>
        </Container>
     )
 }
