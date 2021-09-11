198aa146294321046abd0eb6ab446c2ca923b78f
Karthikeyan <karthikeyan@bangthetable.com>
2021-09-11 23:27:13 +0530

    feat: adding theme ui

---
+++ packages/www/gatsby-browser.js
@@ -0,0 +1,10 @@
+const React = require("react")
+
+// Logs when the client route changes
+exports.onRouteUpdate = ({ location, prevLocation }) => {
+  console.log("new pathname", location.pathname)
+  console.log("old pathname", prevLocation ? prevLocation.pathname : null)
+}
+
+// Wraps every page in a component
+exports.wrapPageElement = require('./wrap-root')
--- packages/www/src/pages/index.js
+++ packages/www/src/pages/index.js
@@ -1,8 +1,11 @@
 import React from 'react'
-
+import { Container, Heading, Button } from '@theme-ui/components';
 const App = () => {
     return(
-        <p> Demo </p>
+       <Container>
+            <Heading as="h1"> Demo </Heading>
+            <Button> Login </Button>
+       </Container>
     )
 }
 
